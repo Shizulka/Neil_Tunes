286719d8a50f4c84169df86838387eab7fb98839
tamanegich <tamanegichu@tuta.io>
2026-03-18 17:40:57 +0200

    fix .env location issues

--- src/Infrastructure/database.py
+++ src/Infrastructure/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-BASE_DIR = Path(__file__).resolve().parent 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env_path = BASE_DIR / ".env"
 
 load_dotenv(dotenv_path=env_path)
